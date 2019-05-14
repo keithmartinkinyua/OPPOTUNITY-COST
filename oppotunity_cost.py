052b999049b3ee1bda013399f747994b1409758f
Keith Martin <keithmartinkinyua@gmail.com>
2019-05-14 06:09:52 +0300

    Add files via upload

---
+++ oppotunity_cost.py
@@ -0,0 +1,44 @@
+import random
+ 
+class oppotunity_cost:
+	#def __init__(self, your_choice):
+	#	self.your_choice
+
+	def make_choice():
+		print("You have the choice of a mango and an apple")
+		for x in range(1):
+			selection = choice(mango, apple)
+			if selection == mango:
+				fruit_value = 0
+			else:
+				fruit_value = 1
+		users_choice = input("Enter your choice now >")
+		return fruit_value
+
+
+	def choice_value(make_choice):
+		value = range(1, 11)
+
+		if fruit_value == 0:
+
+	def loss_or_gain():
+
+	def regret_rate():
+
+
+
+
+
+		#if users_choice == mango
+		#for x in range(1):
+			#users_choice = choice(0, 1)
+
+			#if users_choice == 0
+				#self.your_choice == "mango"
+
+			#elif users_choice == 1
+				#self.your_choice == "apple"
+
+
+		
+		
